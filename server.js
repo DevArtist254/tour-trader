41afc9f68b1c48109b19d2c06f032248e5632cae
devartist <kevinwasonga415@outlook.com>
2023-04-21 19:59:25 +0300

    mvc

--- server.js
+++ server.js
@@ -4,13 +4,58 @@ require("dotenv").config({path: "./config.env"})
 
 const app = express()
 const port = process.env.PORT
+const router = express.Router()
 
+//////////////////////////////////////////////
+//Model
+const tourSchema = mongoose.Schema({
+ name: String,
+ price: Number,
+ summary: String,
+ description: String,
+ createdAt: Date,
+})
+
+const Tour = mongoose.model("Tour", tourSchema)
+
+//////////////////////////////////////////////
+//Middleware
+app.use(express.json())
+
+router.use("/", (req, res, next) => {
+ req.timedNow = new Date().toISOString()
+ next()
+})
+
+router.post("/", async function (req, res) {
+ try {
+  const doc = await Tour.create({
+   name: req.body.name,
+   price: req.body.price,
+   summary: req.body.summary,
+   description: req.body.description,
+   createdAt: req.timedNow,
+  })
+
+  return res.status(200).json({
+   status: "success",
+   data: {
+    doc,
+   },
+  })
+ } catch (error) {
+  return res.status(500).json({
+   status: "error",
+  })
+ }
+})
+
+app.use("/", router)
+
+////////////////////////////////////////////////
+//Server
 mongoose
- .connect(process.env.DB_CONNECT, {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
- })
+ .connect(process.env.DB_CONNECT)
  .then(() => console.log(`DB is connected`))
  .catch((err) => console.error(err))
 
