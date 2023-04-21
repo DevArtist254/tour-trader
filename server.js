62fae98928942286f0685bc22e66a968630b423d
devartist <kevinwasonga415@outlook.com>
2023-04-21 18:19:39 +0300

    init

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 const express = require("express")
+const mongoose = require("mongoose")
+require("dotenv").config({path: "./config.env"})
 
 const app = express()
-const port = 8000 || process.env.PORT
+const port = process.env.PORT
+
+mongoose
+ .connect(process.env.DB_CONNECT, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+ })
+ .then(() => console.log(`DB is connected`))
+ .catch((err) => console.error(err))
 
 app.listen(port, () => {
  console.log(`The server is currently running on port:${port}...`)
